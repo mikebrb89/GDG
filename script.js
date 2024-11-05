59974429affc847743d6864f38d751c973b86a21
mikebrb89 <barbonimichael@gmail.com>
2024-11-05 10:50:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Funzione per scorrere alla sezione selezionata
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').slice(1);
+        document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
